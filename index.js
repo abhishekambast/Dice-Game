c42c0f5027c419acbb79ed4005ebee7a378b4374
abhishekambast <abhishekambast3@gmail.com>
2022-07-11 21:26:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var a=Math.floor(Math.random()*6)+1;
+var r1="images/dice"+a+".png";
+document.querySelector(".img1").setAttribute("src",r1);
+var b=Math.floor(Math.random()*6)+1;
+var r2="images/dice"+b+".png";
+document.querySelector(".img2").setAttribute("src",r2);
+var v=document.querySelectorAll("img");
+if(a<b)document.querySelector("h1").innerHTML="Player 2 Wins";
+else if(b<a)document.querySelector("h1").innerHTML="Player 1 Wins";
+else document.querySelector("h1").innerHTML="Tie";
